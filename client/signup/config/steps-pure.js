1d044d122d3770ba6565a59889aace5b40b1a0f8
ramonjd <ramonjd@gmail.com>
2019-07-25 15:18:02 +1000

    Creating a new step instead of mashing the logic into passwordless Typescripting it :) Updating the bearer_token response value since we're standardizing it in the backend to conform with the users/new response

--- client/signup/config/steps-pure.js
+++ client/signup/config/steps-pure.js
@@ -511,7 +511,7 @@ export function generateSteps( {
 		},
 
 		'create-account': {
-			stepName: 'passwordless',
+			stepName: 'create-account',
 			providesToken: true,
 			providesDependencies: [ 'bearer_token', 'email', 'username' ],
 			unstorableDependencies: [ 'bearer_token' ],
