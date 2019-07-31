15ffe301cff920fabc477149ddeb4c72f3e37c2c
ramonjd <ramonjd@gmail.com>
2019-07-31 11:35:28 +1000

    Reverted `tsx` in chunk path

--- client/signup/config/step-components.js
+++ client/signup/config/step-components.js
@@ -61,7 +61,7 @@ const stepNameToModuleName = {
 export async function getStepComponent( stepName ) {
 	const moduleName = stepNameToModuleName[ stepName ];
 	const module = await import(
-		/* webpackChunkName: "async-load-signup-steps-[request]", webpackInclude: /signup\/steps\/[a-z-]+\/index.(jsx|tsx)$/ */ `signup/steps/${ moduleName }`
+		/* webpackChunkName: "async-load-signup-steps-[request]", webpackInclude: /signup\/steps\/[a-z-]+\/index.jsx$/ */ `signup/steps/${ moduleName }`
 	);
 	return module.default;
 }
